f1f9b41ca769bfea31c6ece0225c121a95716193
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-08-07 10:02:48 +0100

    Configuration of the connection constants

---
+++ SqliteDemo/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace SqliteDemo.Helpers;
+
+public static class Constants
+{
+    private const string DatabaseName = "SqliteDemo.db3";
+
+    public const SQLiteOpenFlags Flags =
+        SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache;
+}
