cc7bf332fe3cfed72329f80ecf84d53a809f7242
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-08-08 12:57:51 +0100

    Creating the customer model

---
+++ SqliteDemo/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace SqliteDemo.Models;
+
+public class Customer
+{
+    public string Name { get; set; } = default!;
+    public string Phone { get; set; } = default!;
+    public int Age { get; set; }
+    public string Address { get; set; } = default!;
+}
