8bdbbfa925798fb5f561d0ccf209334ddf46b828
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-08-09 06:51:00 +0100

    Applying sqlite attributes to a c# class

--- SqliteDemo/Models/Customer.cs
+++ SqliteDemo/Models/Customer.cs
@@ -1,9 +1,20 @@
-﻿namespace SqliteDemo.Models;
+﻿using SQLite;
 
+namespace SqliteDemo.Models;
+
+[Table("Customers")]
 public class Customer
 {
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    [Indexed, NotNull]
     public string Name { get; set; } = default!;
+    [Unique]
     public string Phone { get; set; } = default!;
     public int Age { get; set; }
+    [MaxLength(100)]
     public string Address { get; set; } = default!;
+
+    [Ignore]
+    public bool IsYoung => Age < 50;
 }
